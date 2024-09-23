1df94f171fa96fecb80eb6e24b1fc7c63a48e3f5
oriixlisette <orianalg28@gmail.com>
2024-09-23 20:05:41 -0300

    update new section

--- components/AppSection.vue
+++ components/AppSection.vue
@@ -1,52 +1,98 @@
-<script setup>
+<template>
+  <section class="w-full max-h-screen flex md:py-20">
+    <div :class="[
+      'grid grid-cols-1 grid-rows-2 gap-4 md:grid-cols-2 md:grid-rows-3 md:gap-4',
+      reverse ? 'flex-row-reverse' : ''
+    ]">
 
+<!-- Columna de texto centrado -->
+        <div v-if="secondSection" class="col-span-2 mt-10 md:col-span-3">
+          <p class="text-white font-semibold text-center text-5xl">
+            {{ title }}
+        </p>
+        </div>
+
+      <!-- Columna de texto -->
+      <div :class="[
+        'row-span-2 md:row-span-3',
+        reverse ? 'order-last' : ''
+      ]">
+        <p v-if="!secondSection" class="font-medium text-center text-2xl mt-10 md:text-5xl md:text-left">
+          {{ title }}
+        </p>
+
+        <p class="text-center font-light mb-3 md:text-left">
+          {{ subtitle }}
+        </p>
+        <p class="text-center px-auto md:max-w-xl md:text-left">
+          {{ description }}
+        </p>
+        <p class="text-center px-auto py-6 md:max-w-xl md:text-left">
+          {{ subheading }}
+        </p>
+      </div>
+
+      <!-- Columna de imagen -->
+      <div class="row-span-3 row-start-3 w-full h-full md:row-span-3">
+        <div class="flex justify-center items-center mt-0 mx-0">
+          <!-- Si showSingleImage es true, se muestra una sola imagen -->
+          <img
+            v-if="showSingleImage"
+            :src="images[0]"
+            alt="Single Image"
+            class="w-10/12 h-10/12 mx-auto md:w-8/12 md:h-8/12 md:mx-0"
+          />
+          <!-- Si showSingleImage es false, se muestran dos imágenes -->
+          <div v-else class="flex flex-col space-x-0 md:flex-row md:space-x-4">
+            <img
+              v-for="(image, index) in images.slice(0, 2)"
+              :key="index"
+              :src="image"
+              :alt="`Image ${index + 1}`"
+              class="w-12/12 h-12/12 mx-auto md:w-5/12 md:h-5/12 md:mx-0"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
 defineProps({
   title: {
     type: String,
-    required: true  
+    required: true
   },
   subtitle: {
     type: String,
-    required: false  
+    required: false
   },
-   description: {
+  description: {
     type: String,
     required: true
   },
-images: {
-    type: [String, Array],
+  subheading: {
+    type: String,
+    required: false
+  },
+  images: {
+    type: Array,
     required: true
   },
+  reverse: {
+    type: Boolean,
+    default: false
+  },
+  showSingleImage: {
+    type: Boolean,
+    default: true
+  },
+    secondSection: {
+    type: Boolean,
+    default: true
+  }
 })
 
+
 </script>
-
-<template>
-<section class="w-full max-h-screen md:py-20">
-  <div class="grid grid-cols-1 grid-rows-2 gap-4 md:grid-cols-2 md:grid-rows-3 md:gap-4 ">
-    
-    <div class="row-span-2 md:row-span-3">
-      <p class="font-medium text-center text-2xl mt-10 md:text-5xl md:text-left">
-   {{ title }}
-    </p>
-    <p class="text-center font-light mb-3 md:text-left ">
-   {{ subtitle }}
-    </p>
-    <p class="text-center px-auto md:max-w-xl md:text-left">
-   {{ description.toString() }}
-    </p>
-    </div>
-
-    <div class="row-span-3 row-start-3 w-full h-full md:row-span-3">
-      <div class="flex justify-center items-center mt-0 mx-0">
-        <img
-          :src="images"
-          alt="Picture of women using filters on Instagram" 
-          class="w-10/12 h-10/12 mx-auto md:w-8/12 md:h-8/12 md:mx-0" />
-       </div>
-    </div>
-</div>
-
-
-</section>
-</template> 
--- pages/index.vue
+++ pages/index.vue
@@ -16,14 +16,15 @@ import presaleEvent from "./presale-event.vue";
   <!-- SECTION 1 COUNTER -->
  <presaleEvent/>
 
-
-
   <!-- SECTION 2 -->
   <div class="default-section-p bg-[#D15D52] text-white ">
     <AppSection
+      reverse 
       title="OBTENÉ LOS NUEVOS STICKERS"
       description="Disfruta de stickers sobre maquillaje que te permite favorecer la identidad de tu marca."
-      images="/img/stickers00.png"
+      :images="['/img/stickers00.png']"
+      :showSingleImage="true"
+      :secondSection="false"
     />
   </div>
 
@@ -32,13 +33,36 @@ import presaleEvent from "./presale-event.vue";
 <CaptureAudience />
 </div>
 
-<div class="default-section-p bg-[#252525] text-white ">
+<div class="default-section-p bg-[#252525] text-white">
     <AppSection
       title="LOS MEJORES FILTROS EN REDES SOCIALES"
       description="Cientos de Influencers y marcas de maquillaje utilizan los filtros para ofrecer mejor contenido a sus seguidores ya que les permite conectar mejor con su audiencia."
-      images="/img/stickers03.png"
+      :images="['/img/stickers03.png']"
+      :showSingleImage="true"
+      :secondSection="false"
     />
   </div> 
+
+  
+<div class="default-section-p bg-[#252525] text-white">
+    <AppSection
+      title="COMUNICA MEJOR"
+      description="Logran diversidad en tus stories, posts."
+      subheading="Incrementan el número de vistas y comentarios que reciben tus publicaciones."
+      :images="['/img/stickers05.png', '/img/stickers06.png']"
+      reverse
+      :showSingleImage="false"
+      :secondSection="true"
+    />
+  </div> 
+
+ <!--  <div class="default-section-p bg-[#252525] text-white">
+    <ComunicateBetter
+      title="LOS MEJORES FILTROS EN REDES SOCIALES"
+      description="Cientos de Influencers y marcas de maquillaje utilizan los filtros para ofrecer mejor contenido a sus seguidores ya que les permite conectar mejor con su audiencia."
+      :images="require('~/assets/img/stickers05.png')"
+    />
+  </div>  -->
 
 
 </template>
